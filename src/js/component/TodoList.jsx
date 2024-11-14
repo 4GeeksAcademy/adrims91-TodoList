14ed8e7c02df4103d2e424f96317112bf0131893
adrims91 <adrims91@gmail.com>
2024-11-14 11:04:31 +0000

    added doneTask state

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -8,19 +8,30 @@ const TodoList = () => {
 	
     const [inputValue, setInputValue] = useState("");
     const [arrInput, setArrInput] = useState([])
+    const [doneTasks, setDoneTasks] = useState([])
 
 
     const addTask = () => {
         if (inputValue === '') return;
-        setArrInput([...arrInput, inputValue])
-        setInputValue('')
+        setArrInput([...arrInput, inputValue]);
+        setDoneTasks([...doneTasks, false]);
+        setInputValue('');
     }
     
     
     const deleteTask = (index) => {
-        const newArr = arrInput.filter((e,i) => i !== index)
-        setArrInput(newArr)
+        const newArr = arrInput.filter((_, i) => i !== index);
+        const newDoneTasks = doneTasks.filter((_, i) => i !== index);
+        setArrInput(newArr);
+        setDoneTasks(newDoneTasks);
     }
+
+    const closeTask = (index) => {
+        const newDoneTasks = [...doneTasks];
+        newDoneTasks[index] = !newDoneTasks[index];
+        setDoneTasks(newDoneTasks);
+    }
+
 
     return (
         <>
@@ -40,12 +51,12 @@ const TodoList = () => {
                 />
                 {arrInput.map((item, index)=> (
                     <li className="list-group-item" key={index}>
-                       <p className="fs-5">{item}  <i onClick={() => deleteTask(index)} className="fa-solid fa-x float-end"></i>
+                       <p onClick={() => closeTask(index)} className={`${doneTasks[index] === true ? 'text-success' : ''} fs-5`}>{item} {doneTasks[index] === true ? <i class="fa-solid fa-check"></i> : ''}  <i onClick={() => deleteTask(index)} className="fa-solid fa-x float-end"></i>
                        </p>
                     </li>
                 ))}
                 <li className="list-group-item">
-                    <label className={arrInput.length === 0 ? 'text-danger' : 'text-success'} style={{fontSize: '11px'}}>{arrInput.length === 0 ? 'There are no tasks, add a new task.' : `${arrInput.length} items left.`}</label>
+                    <label className={arrInput.length === 0 ? 'text-danger' : 'text-success'} style={{fontSize: '11px'}}>{arrInput.length === 0  ? 'There are no tasks, add a new task.' : `${arrInput.length} items left.`}</label>
                 </li>
                 </ul>
             </div>
